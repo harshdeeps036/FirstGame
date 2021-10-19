b05969cb41e15f8947e7989c71ed430916d626e3
Harshdeep Singh <harshdeeps036@gmail.com>
2021-10-19 17:34:19 +0530

    4

--- main.py
+++ main.py
@@ -7,10 +7,26 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 running = True
+
+#Title
+
+pygame.display.set_caption('The Harsh Game')
+
+#Icon
+
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+#The main Game Loop
+
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+    
+
+
+
 
 
 
