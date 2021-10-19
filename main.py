24978ddc8de1c182bcb4ba44f866ee4347faadb0
Harshdeep Singh <harshdeeps036@gmail.com>
2021-10-19 17:27:54 +0530

    2

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ pygame.init()
 #screen
 screen = pygame.display.set_mode((800,600))
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+
 
 
 
