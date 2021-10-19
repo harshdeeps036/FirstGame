700cfd0b5ae18a3ee0c49575feb750d2da6caafd
Harshdeep Singh <harshdeeps036@gmail.com>
2021-10-19 17:38:32 +0530

    5

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+
+    screen.fill((129,129,150))
+    pygame.display.update()
     
 
 
