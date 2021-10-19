a69a589203fec1e7c050e5cef0bbeab6a7eea38f
Harshdeep Singh <harshdeeps036@gmail.com>
2021-10-19 18:05:09 +0530

    8

--- main.py
+++ main.py
@@ -20,22 +20,49 @@ pygame.display.set_icon(icon)
 #Player
 
 playerimg = pygame.image.load('player.png')
-playerX = 390
+playerX = 380
 playerY = 480
+xchange = 0
+ychange = 0
 
-def player():
-    screen.blit(playerimg,(playerX, playerY))
+def player(x, y):
+    screen.blit(playerimg,(x, y))
 
 #The main Game Loop
 
 while running:
+
+    screen.fill((129,129,150))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
 
-    screen.fill((129,129,150))
-    player()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                xchange = -0.1
+            if event.key == pygame.K_RIGHT:
+                xchange = 0.1
+            if event.key == pygame.K_UP:
+                ychange = -0.1
+            if event.key == pygame.K_DOWN:
+                ychange = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                xchange = 0
+            if event.key == pygame.K_RIGHT:
+                xchange = 0      
+            if event.key == pygame.K_UP:
+                ychange = 0
+            if event.key == pygame.K_DOWN:
+                ychange = 0          
+    
+    playerX += xchange
+    playerY += ychange
+    player(playerX, playerY)
+
     pygame.display.update()
+
+
     
 
 
