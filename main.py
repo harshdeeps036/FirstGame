429b09a798e4fbe4f6c7afddd8d14d5c1fe4a4b7
Harshdeep Singh <harshdeeps036@gmail.com>
2021-10-19 17:48:11 +0530

    7

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ pygame.display.set_caption('The Harsh Game')
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+#Player
+
+playerimg = pygame.image.load('player.png')
+playerX = 390
+playerY = 480
+
+def player():
+    screen.blit(playerimg,(playerX, playerY))
+
 #The main Game Loop
 
 while running:
@@ -25,6 +34,7 @@ while running:
             running=False
 
     screen.fill((129,129,150))
+    player()
     pygame.display.update()
     
 
